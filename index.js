f3705f6d04b92e035b3a1ce0adf4cba9aafdba26
veermove <veermove@users.noreply.github.com>
2021-09-18 18:59:51 +0200

    tests

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const canvasSizeX = 1000;
 const canvasSizeY = canvasSizeX;
 const canvasPosX = 0;
 const canvasPosY = 0;
-
-const breakpoint = 8;
 const pointsNumber = 500;
+
+const minDist = 0;
+
+const searchSize = 1;
+// searchSize, SquaresLookedAt
+//     1:      3x3
+//     2:      5x5
+//     3:      7x7
+//     ...
+let indexingHelper = (searchSize * 2 ) + 1;
 
 let canvas = d3.select("body")
     .append("svg")
@@ -13,8 +21,7 @@ let canvas = d3.select("body")
 
 
 const main = () => {
-
-    // generate scatter points and save them in an array
+    // generate scattered points and save them in an array
     let points = [];
     for (let i = 0; i < pointsNumber; i++) {
         let p = {};
@@ -31,74 +38,50 @@ const main = () => {
     let t0 = performance.now();
     let grid = generateGrid(points);
 
+
     let connected = points.slice();
     connected.forEach(point => {
         findNearestAndDraw(grid, point);
     });
-
     let t1 = performance.now();
-
 
     console.log("time: " + (t1 - t0) + "ms");
 }
 
-const generateGrid = (setOfPoints) => {
-    // for n points grid should consist of z = sqrt(n) squares,
-    // so grid side should consist of sqrt(z) squares
-    let gridSize = Math.ceil( Math.sqrt( Math.sqrt( pointsNumber ) ) );
 
-    // generate 2-dimensional sqaure array with side length of gridSize
-    let grid = new Array(gridSize);
-    for (let i = 0; i < gridSize; i++) {
-        grid[i] = new Array(gridSize);
-    }
 
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            grid[i][j] = {};
+// const findNearestAndDraw = (grid, currentPoint) => {
 
-            // define boundaries of each sqaure
-            grid[i][j].xStart = canvasPosX + (i * (canvasSizeX / gridSize));
-            grid[i][j].xEnd = canvasPosX + ( (i + 1)  * (canvasSizeX / gridSize));
+//     let lines = [];
 
-            grid[i][j].yStart = canvasPosY + (j * (canvasSizeY / gridSize));
-            grid[i][j].yEnd = canvasPosY + ( (j + 1)  * (canvasSizeY / gridSize));
+//     // search around square of current point
+//     let xAddres = Math.floor( currentPoint.x / (canvasSizeX / grid.length));
+//     let yAddres = Math.floor( currentPoint.y / (canvasSizeY / grid.length));
 
-            // allow points to be stored inside each sqaure
-            grid[i][j].pointsInside = [];
-        }
-    }
+//     function find(startingSearchSize) {
 
-    function findPlace(point) {
-        // find place for given point in the grid
-        let xAddres = Math.floor( point.x / (canvasSizeX / gridSize));
-        let yAddres = Math.floor( point.y / (canvasSizeY / gridSize));
+//     }
+// }
 
-        // store given point in the grid
-        grid[xAddres][yAddres].pointsInside.push(point);
-    }
-
-    // place each point in the grid
-    setOfPoints.forEach(point => findPlace(point));
-
-    return grid;
-}
 
 const findNearestAndDraw = (grid, currentPoint) => {
+
     let distances = [];
 
     // search around square of current point
     let xAddres = Math.floor( currentPoint.x / (canvasSizeX / grid.length));
     let yAddres = Math.floor( currentPoint.y / (canvasSizeY / grid.length));
-    for (let dx = -1; dx < 2; dx++) {
-        for (let dy = -1; dy < 2; dy ++) {
-            if (!grid[xAddres + dx] || !grid[xAddres + dx][yAddres + dy]) { continue; }// border cases
+
+    for (let dx = -searchSize; dx < indexingHelper; dx++) {
+        for (let dy = -searchSize; dy < indexingHelper; dy ++) {
+            // ignore border cases
+            if (!grid[xAddres + dx] || !grid[xAddres + dx][yAddres + dy]) { continue; }
 
             grid[xAddres + dx][yAddres + dy].pointsInside.forEach(point => {
                 let distPoint = {};
                 distPoint.p = point;
                 distPoint.dist = calcDistance(currentPoint, point);
-                if (distPoint.dist !== 0) {
+                if (distPoint.dist > minDist) {
                     distances.push(distPoint);
                 }
             });
@@ -137,6 +120,52 @@ const calcDistance = (pointA, pointB) => {
     let xDif = (pointA.x - pointB.x);
     let yDif = (pointA.y - pointB.y);
     return Math.sqrt( (xDif * xDif) + (yDif * yDif) );
+}
+
+const generateGrid = (setOfPoints) => {
+    // for n points grid should consist of z = sqrt(n) squares,
+    // so grid side should consist of sqrt(z) squares
+    let gridSize = Math.ceil( Math.sqrt( Math.sqrt( pointsNumber ) ) );
+    console.log('N x N where n = ' + gridSize);
+
+    // generate 2-dimensional sqaure array with side length of gridSize
+    let grid = new Array(gridSize);
+    for (let i = 0; i < gridSize; i++) {
+        grid[i] = new Array(gridSize);
+    }
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            grid[i][j] = {};
+
+            // define boundaries of each sqaure
+            grid[i][j].xStart = canvasPosX + (i * (canvasSizeX / gridSize));
+            grid[i][j].xEnd = canvasPosX + ( (i + 1)  * (canvasSizeX / gridSize));
+
+            grid[i][j].yStart = canvasPosY + (j * (canvasSizeY / gridSize));
+            grid[i][j].yEnd = canvasPosY + ( (j + 1)  * (canvasSizeY / gridSize));
+
+            // allow points to be stored inside each sqaure
+            grid[i][j].pointsInside = [];
+        }
+    }
+
+    let xHelp = (canvasSizeX / gridSize);
+    let yHelp = (canvasSizeY / gridSize);
+
+    function findPlace(point) {
+        // find place for given point in the grid
+        let xAddres = Math.floor( point.x / xHelp);
+        let yAddres = Math.floor( point.y / yHelp);
+
+        // store given point in the grid
+        grid[xAddres][yAddres].pointsInside.push(point);
+    }
+
+    // place each point in the grid
+    setOfPoints.forEach(point => findPlace(point));
+
+    return grid;
 }
 
 const drawCircle = (x, y) => {
