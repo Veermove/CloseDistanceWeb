7c5394402ab30a96f1f3cd47d36b96c80a0e0345
veermove <veermove@users.noreply.github.com>
2021-09-20 22:29:14 +0200

    base animation added

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ let indexingHelper = (searchSize * 2 ) + 1;
 //     3:      7x7
 //     ...
 
+const maxPointVelocity = 0.1;
+const maxLineLength = 150;
 
 let canvas = d3.select("body")
     .append("svg")
@@ -37,6 +39,10 @@ const main = () => {
         let p = {};
         p.x = ( Math.random() * canvasSizeX);
         p.y = ( Math.random() * canvasSizeY);
+        p.velX = ( (Math.random() * ( 2 * maxPointVelocity ) - maxPointVelocity) );
+        p.velY = ( (Math.random() * ( 2 * maxPointVelocity ) - maxPointVelocity) );
+        // p.velX = ( (Math.random() * maxPointVelocity) );
+        // p.velY = ( (Math.random() * maxPointVelocity) );
         points.push(p);
     }
     // draw points from array and connect them to SVG circles
@@ -55,8 +61,6 @@ const main = () => {
     //            .attr("cy", (d) => { return d.y; });
     // ADD ALL CHANGED ATTRIBUTES
 
-    let t0 = performance.now();
-
     // generate grid and fill it with points from array
     let grid = generateGrid(points);
 
@@ -70,7 +74,7 @@ const main = () => {
     });
 
     // draw lines from array and connect them to SVG lines
-    let line = canvas.selectAll("line")
+    let Lines = canvas.selectAll("line")
                 .data(allLines)
                 .enter()
                 .append("line")
@@ -81,8 +85,63 @@ const main = () => {
                     .attr("stroke", "red")
                     .attr("stroke-width", 1);
 
-    let t1 = performance.now();
-    console.log("time: " + (t1 - t0) + "ms");
+    let interval = 45;
+    setInterval(() => {
+        if (window.dontDoIt) return;
+
+        let t0 = performance.now();
+
+        function resetGrid() {
+            for (let i = 0; i < grid.length; i++) {
+                for (let j = 0; j < grid.length; j++) {
+                    // reset each square
+                    grid[i][j].pointsInside = [];
+                }
+            }
+        }
+        resetGrid();
+
+        allLines = [];
+
+        points.forEach(point => {
+            point.x = (point.x + (point.velX * interval)) % canvasSizeX;
+            point.y = (point.y + (point.velY * interval)) % canvasSizeY;
+
+            let xHelp = (canvasSizeX / grid.length);
+            let yHelp = (canvasSizeY / grid.length);
+
+            // find place for given point in the grid
+            let xAddres = (Math.floor( point.x / xHelp) + grid.length) % grid.length;
+            let yAddres = (Math.floor( point.y / yHelp) + grid[0].length) % grid[0].length;
+
+            // store given point in the grid
+            grid[xAddres][yAddres].pointsInside.push(point);
+            let addition = findNearest(grid, point)
+
+            allLines = allLines.concat( addition );
+        });
+
+        Circles.attr("cx", (d) => { return d.x; })
+               .attr("cy", (d) => { return d.y; });
+
+
+        canvas.selectAll("line").remove();
+        allLines.forEach(line => {
+            if (calcDistance(line.start, line.end) < maxLineLength) {
+                let lix = canvas.append("line")
+                .attr("x1", line.start.x)
+                .attr("y1", line.start.y)
+                .attr("x2", line.end.x)
+                .attr("y2", line.end.y)
+                .attr("stroke", "red")
+                .attr("stroke-width", 1);
+
+            }
+        });
+
+        let t1 = performance.now();
+        console.log("time: " + (t1 - t0) + "ms");
+    }, interval);
 }
 
 // returns array of lines to be drawn from current point
@@ -91,29 +150,40 @@ const findNearest = (grid, currentPoint) => {
 
     let distances = [];
 
-    // search around square of current point
+    // find addres of current point
     let xAddres = Math.floor( currentPoint.x / (canvasSizeX / grid.length));
     let yAddres = Math.floor( currentPoint.y / (canvasSizeY / grid.length));
 
+    // search around square of current point
     for (let dx = -searchSize; dx < indexingHelper; dx++) {
         for (let dy = -searchSize; dy < indexingHelper; dy ++) {
 
             // ignore border cases
             if (!grid[xAddres + dx] || !grid[xAddres + dx][yAddres + dy]) { continue; }
 
+            // console.log('pointsInsideLengt ' + grid[xAddres + dx][yAddres + dy].pointsInside.length);
+            if (!grid[xAddres + dx][yAddres + dy].pointsInside.length) {continue; }
+
             // for each point in current square calculate and store distance to given point
             grid[xAddres + dx][yAddres + dy].pointsInside.forEach(point => {
-                let distPoint = {};
-                distPoint.p = point;
-                distPoint.dist = calcDistance(currentPoint, point);
-                if (distPoint.dist > minDist) {
-                    distances.push(distPoint);
+                let distance = calcDistance(currentPoint, point);
+                if (distance) {
+                    let distPoint = {};
+                    distPoint.p = point;
+                    distPoint.dist = distance
+                    if (distPoint.dist > minDist) {
+                        distances.push(distPoint);
+                    }
                 }
             });
         }
     }
 
-    // find n smallest values by distance
+
+
+    if (distances.length == 0 || distances[0].dist == 0) { return []; }
+    // if (!distances[0]) { console.log('HUJ'); return []; }
+
     let pointsToDraw = [];
     for (let i = 0; i < n; i++) {
 
@@ -128,6 +198,9 @@ const findNearest = (grid, currentPoint) => {
         }
         pointsToDraw.push(distances[minIndex].p);
         distances.splice(minIndex, 1);
+        if (distances.length === 0) {
+            break;
+        }
     }
 
     // Create array of lines to be drawn
