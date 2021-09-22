4ea4242051ccf9a71444ee6b16f5a2c25a4f8376
veermove <veermove@users.noreply.github.com>
2021-09-22 18:48:28 +0200

    v2.1

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // canvas size
-const canvasSizeX = 1000;
-const canvasSizeY = canvasSizeX;
-
-// canvas position/offset
-const canvasPosX = 0;
-const canvasPosY = 0;
+const canvasSizeX = document.querySelector('#background').offsetWidth;
+const canvasSizeY = document.querySelector('#background').offsetHeight;
 
 // number of points to be generated
-const pointsNumber = 250;
+const pointsNumber = 200;
 
 // number of lines drawn from each point
 const n = 4;
@@ -24,15 +20,16 @@ let indexingHelper = (searchSize * 2 ) + 1;
 //     3:      7x7
 //     ...
 
-const maxPointVelocity = 0.1;
-const maxLineLength = 150;
+const maxPointVelocity = 0.06;
+const maxLineLength = canvasSizeX * 0.15;
 
-let canvas = d3.select("body")
+let canvas = d3.select("#background")
     .append("svg")
     .attr("width", canvasSizeX)
     .attr("height", canvasSizeY);
 
 const main = () => {
+
     // generate scattered points and save them in an array
     let points = [];
     for (let i = 0; i < pointsNumber; i++) {
@@ -43,6 +40,7 @@ const main = () => {
         p.velY = ( (Math.random() * ( 2 * maxPointVelocity ) - maxPointVelocity) );
         points.push(p);
     }
+
     // draw points from array and connect them to SVG circles
     let Circles = canvas.selectAll("cricle")
                         .data(points)
@@ -53,58 +51,84 @@ const main = () => {
                             .attr("fill", "red")
                             .attr("r", 3);
 
-    // NOTE TO SELF
-    // TO REDRAW IE. SET OF POINTS USE
-    //     Circles.attr("cx", (d) => { return d.x; })
-    //            .attr("cy", (d) => { return d.y; });
-    // ADD ALL CHANGED ATTRIBUTES
-
     // generate grid and fill it with points from array
     let grid = generateGrid(points);
 
-    // for each point calcualte all outgoing lines
-    // calculated lines store in '.connect' array attribute
-    let allLines = [];
-    points.forEach(point => {
-        allLines.push( findNearest(grid, point) );
-    });
+    // create color gradient based on distance
+    let colorGrad = d3.scaleLinear().domain([0, maxLineLength]).range(["red", "blue"]);
 
-    let colorGrad = d3.scaleLinear().domain([0, 200]).range(["red", "blue"]);
+    // function to redrawLines called every frame
+    // =======================================================================================
+    function redrawLines() {
+        // for each point:
+        points.forEach(point => {
+            // create array of points that should be connected to current point
+            point.connect = [];
+            point = findNearest(grid, point);
 
-    // draw lines from array
-    allLines.forEach(point => {
-        point.connect.forEach(endPoint => {
-            let Lines = canvas.append("line")
-                    .attr("x1", point.x)
-                    .attr("y1", point.y)
-                    .attr("x2", endPoint.x)
-                    .attr("y2", endPoint.y)
-                    .attr("stroke", colorGrad(calcDistance(point, endPoint)))
-                    .attr("stroke-width", 1);
+            // store sum of distances to each point that will be connected to current point
+            let sumOfDistances = 0;
+
+            // for each point that current point will be connected to:
+            point.connect.forEach(endPoint => {
+                // calculate distance to current point
+                let dist = calcDistance(point, endPoint);
+
+                // if distance from current point to endPoint is greater
+                // than some const value - don't draw that line
+                if (dist < maxLineLength)
+                {
+                    // add distance to current point to the sum
+                    sumOfDistances += dist;
+                    let Lines = canvas.append("line")
+                        .attr("x1", point.x)
+                        .attr("y1", point.y)
+                        .attr("x2", endPoint.x)
+                        .attr("y2", endPoint.y)
+                        .attr("stroke", colorGrad(dist))
+                        .attr("stroke-width", 1);
+                }
+            });
+
+            // if findNearest(...) doesn't find any points for current point
+            // to be connected to, point.connect array is empty
+            if (!point.connect.length) {
+                // hence treat it's average distance to other points as maximum
+                point.avgDist = maxLineLength;
+            } else {
+                // else calcualte average distance to points connected to it
+                point.avgDist = sumOfDistances / point.connect.length;
+            }
         });
-    });
+    }
+    // =======================================================================================
 
-
+    redrawLines();
 
     window.dontDoIt = true;
     let interval = 45;
     setInterval(() => {
         if (!window.dontDoIt) return;
 
-        function resetGrid() {
-            for (let i = 0; i < grid.length; i++) {
-                for (let j = 0; j < grid.length; j++) {
-                    // reset each square
-                    grid[i][j].pointsInside = [];
-                }
+        // reset grid
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                // reset each square
+                grid[i][j].pointsInside = [];
             }
         }
-        resetGrid();
+
+        // remove all drawn lines
         canvas.selectAll("line").remove();
+
+        // for each point update it's position and place it in a grid
         points.forEach(point => {
 
+            // calculate difference in position
             let DeltaX = point.velX * interval;
             let DeltaY = point.velY * interval;
+
+            // update position
             point.x =  ((point.x + DeltaX) + canvasSizeX) % canvasSizeX ;
             point.y =  ((point.y + DeltaY) + canvasSizeY) % canvasSizeY ;
 
@@ -119,32 +143,13 @@ const main = () => {
             grid[xAddres][yAddres].pointsInside.push(point);
         });
 
+        // call function to re-draw all lines with updated position and color
+        redrawLines();
+
+        // re-draw points with updated position and color
         Circles.attr("cx", (d) => { return d.x; })
-               .attr("cy", (d) => { return d.y; });
-
-        allLines = [];
-        points.forEach(point => {
-            point.connect = [];
-            allLines.push( findNearest(grid, point) );
-        });
-
-        // draw lines from array
-        allLines.forEach(point => {
-            point.connect.forEach(endPoint => {
-                let dist = calcDistance(point, endPoint)
-                if (dist < maxLineLength)
-                {
-                    let Lines = canvas.append("line")
-                        .attr("x1", point.x)
-                        .attr("y1", point.y)
-                        .attr("x2", endPoint.x)
-                        .attr("y2", endPoint.y)
-                        .attr("stroke", colorGrad(dist))
-                        .attr("stroke-width", 1);
-                }
-            });
-        });
-
+               .attr("cy", (d) => { return d.y; })
+               .attr("fill", (d) => { return colorGrad(d.avgDist); });
 
     }, interval);
 }
@@ -226,7 +231,6 @@ const generateGrid = (setOfPoints) => {
     // for n points grid should consist of z = sqrt(n) squares,
     // so grid side should consist of sqrt(z) squares
     let gridSize = Math.ceil( Math.sqrt( Math.sqrt( pointsNumber ) ) );
-    console.log('N x N where n = ' + gridSize);
 
     // generate 2-dimensional sqaure array with side length of gridSize
     let grid = new Array(gridSize);
@@ -237,14 +241,8 @@ const generateGrid = (setOfPoints) => {
     // for all squares in grid:
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
+            // create empty
             grid[i][j] = {};
-
-            // define boundaries of each sqaure
-            grid[i][j].xStart = canvasPosX + (i * (canvasSizeX / gridSize));
-            grid[i][j].xEnd = canvasPosX + ( (i + 1)  * (canvasSizeX / gridSize));
-
-            grid[i][j].yStart = canvasPosY + (j * (canvasSizeY / gridSize));
-            grid[i][j].yEnd = canvasPosY + ( (j + 1)  * (canvasSizeY / gridSize));
 
             // allow points to be stored inside each sqaure
             grid[i][j].pointsInside = [];
