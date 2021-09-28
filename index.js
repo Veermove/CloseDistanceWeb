2cc1479d60d8309fa5bcedc9c30076c728678c2d
veermove <veermove@users.noreply.github.com>
2021-09-28 17:37:13 +0200

    changed 'line' storage to adjacency matrix

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ const canvasSizeY = document.querySelector('#background').offsetHeight;
 const pointsNumber = 200;
 
 // number of lines drawn from each point
-const n = 4;
+const n = 5;
 
 // ignore points with distance (from currentPoint when calculating outgoing lines)
 // smaller than minDist
 const minDist = 0;
 
+// how many squares in a grid are checked
 const searchSize = 1;
-let indexingHelper = (searchSize * 2 ) + 1;
 // searchSize, SquaresLookedAt
 //     1:      3x3
 //     2:      5x5
 //     3:      7x7
 //     ...
 
+let indexingHelper = (searchSize * 2 ) + 1;
+
+// max point velocity in px/ms
 const maxPointVelocity = 0.06;
-const maxLineLength = canvasSizeX * 0.15;
+
+const maxLineLength = (canvasSizeX * 0.15);
 
 let canvas = d3.select("#background")
     .append("svg")
@@ -34,8 +38,15 @@ const main = () => {
     let points = [];
     for (let i = 0; i < pointsNumber; i++) {
         let p = {};
+
+        // store index its index in array
+        p.index = i;
+
+        // generate rnadom position
         p.x = ( Math.random() * canvasSizeX);
         p.y = ( Math.random() * canvasSizeY);
+
+        // generate random velocity
         p.velX = ( (Math.random() * ( 2 * maxPointVelocity ) - maxPointVelocity) );
         p.velY = ( (Math.random() * ( 2 * maxPointVelocity ) - maxPointVelocity) );
         points.push(p);
@@ -57,60 +68,104 @@ const main = () => {
     // create color gradient based on distance
     let colorGrad = d3.scaleLinear().domain([0, 0.7 * maxLineLength]).range(["red", "blue"]);
 
-    // function to redrawLines called every frame
-    // =======================================================================================
-    function redrawLines() {
+    // create empty adjecency matrix
+    let adjMatrix = new Array(pointsNumber);
+
+    for (let i = 0; i < pointsNumber; i++) {
+        adjMatrix[i] = new Array(pointsNumber);
+    }
+
+    // redraw all lines based on adjacency matrix
+    // ===========================================================================
+    function redrawLines(matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            // count outgoing lines from current point
+            let currentCount = 0;
+
+            // store sum of distances from current point to end points
+            let sumOfDist = 0;
+            for (let j = i; j < matrix.length; j++) {
+
+                // if two points should connect to a line
+                if (matrix[i][j]) {
+                    currentCount++;
+
+                    // calcualte length of this line and store it
+                    let dist = calcDistance(points[i], points[j]);
+                    sumOfDist += dist;
+
+                    // draw line
+                    let lines = canvas.append("line")
+                        .attr("x1", points[i].x)
+                        .attr("y1", points[i].y)
+                        .attr("x2", points[j].x)
+                        .attr("y2", points[j].y)
+                        .attr("stroke", colorGrad(dist))
+                        .attr("stroke-width", 1);
+                }
+            }
+
+            // // if findNearest(...) doesn't find any points for current point
+            // // to be connected to, point.connect array is empty
+            // if (false) {
+            //     // hence treat it's average distance to other points as maximum
+            //     points[i].avgDist = maxLineLength;
+            // } else {
+            //     // for each point calculate average distance to 4 nearest
+            //     // points connected to it
+            // points[i].avgDist = sumOfDist / currentCount;
+            // }
+        }
+    }
+
+    // in each point store references to points this point should be connected to
+    // ===========================================================================
+    function findConnections() {
         // for each point:
         points.forEach(point => {
             // create array of points that should be connected to current point
             point.connect = [];
+
+            // find nearest points, and put them in the .connect array
             point = findNearest(grid, point);
-
-            // store sum of distances to each point that will be connected to current point
-            let sumOfDistances = 0;
-
-            // for each point that current point will be connected to:
-            point.connect.forEach(endPoint => {
-                // calculate distance to current point
-                let dist = calcDistance(point, endPoint);
-
-                // if distance from current point to endPoint is greater
-                // than some const value - don't draw that line
-                if (dist < maxLineLength)
-                {
-                    // add distance to current point to the sum
-                    sumOfDistances += dist;
-                    let Lines = canvas.append("line")
-                        .attr("x1", point.x)
-                        .attr("y1", point.y)
-                        .attr("x2", endPoint.x)
-                        .attr("y2", endPoint.y)
-                        .attr("stroke", colorGrad(dist))
-                        .attr("stroke-width", 1);
-                }
-            });
-
-            // if findNearest(...) doesn't find any points for current point
-            // to be connected to, point.connect array is empty
-            if (!point.connect.length) {
-                // hence treat it's average distance to other points as maximum
-                point.avgDist = maxLineLength;
-            } else {
-                // else calcualte average distance to points connected to it
-                point.avgDist = sumOfDistances / point.connect.length;
-            }
         });
     }
-    // =======================================================================================
 
-    redrawLines();
+    // calculate new position of a given point, returns updated point
+    // ===========================================================================
+    function updatePoint(point) {
+        // calculate difference in position
+        let DeltaX = point.velX * interval;
+        let DeltaY = point.velY * interval;
 
-    window.dontDoIt = true;
+        // update position
+        point.x =  ((point.x + DeltaX) + canvasSizeX) % canvasSizeX ;
+        point.y =  ((point.y + DeltaY) + canvasSizeY) % canvasSizeY ;
+
+        return point;
+    }
+
+    // for all points find its 4 nearest neighbours
+    findConnections();
+
+    // create and fill adjacency matrix
+    adjMatrix = checkMatrixValidity(setAdjMatrix(adjMatrix, points));
+
+
+    // call function to redraw lines from adjacency matrix
+    redrawLines(adjMatrix);
+
+    // pause condition to stop simulation via console
+    window.pause = false;
+
     let interval = 45;
-    setInterval(() => {
-        if (!window.dontDoIt) return;
 
-        // reset grid
+    setInterval(() => {
+        if (window.pause) {
+            return;
+        }
+
+        // clear each square of grid
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid.length; j++) {
                 // reset each square
@@ -124,13 +179,8 @@ const main = () => {
         // for each point update it's position and place it in a grid
         points.forEach(point => {
 
-            // calculate difference in position
-            let DeltaX = point.velX * interval;
-            let DeltaY = point.velY * interval;
-
-            // update position
-            point.x =  ((point.x + DeltaX) + canvasSizeX) % canvasSizeX ;
-            point.y =  ((point.y + DeltaY) + canvasSizeY) % canvasSizeY ;
+            // call function to update position
+            updatePoint(point);
 
             let xHelp = (canvasSizeX / grid.length);
             let yHelp = (canvasSizeY / grid.length);
@@ -143,15 +193,88 @@ const main = () => {
             grid[xAddres][yAddres].pointsInside.push(point);
         });
 
-        // call function to re-draw all lines with updated position and color
-        redrawLines();
+        // for all points
+        //     find nearest point to current
+        findConnections();
 
+        // calculate new adjacency matrix
+        // adjMatrix = checkMatrixValidity(setAdjMatrix(adjMatrix, points));
+        adjMatrix = setAdjMatrix(adjMatrix, points);
+
+        // call function to re-draw all lines with updated position and color from adjacency matrix
+        redrawLines(adjMatrix);
+
+        points = calculateAvgDistances(adjMatrix, points);
         // re-draw points with updated position and color
         Circles.attr("cx", (d) => { return d.x; })
                .attr("cy", (d) => { return d.y; })
                .attr("fill", (d) => { return colorGrad(d.avgDist); });
 
     }, interval);
+}
+
+// set adjacency matrix to be false at every adress
+// returns 'empty' adjacency matrix
+function resetAdjMatrix(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            matrix[i][j] = false;
+        }
+    }
+    return matrix;
+}
+
+// fill adjacency matrix from adjacency list (here adjacency array)
+// returns 'filled' matrix
+function setAdjMatrix(matrix, setOfPoints) {
+
+    // reset matrix before filling
+    matrix = resetAdjMatrix(matrix);
+
+    // for all points in a given set of points
+    for (let i = 0; i < setOfPoints.length; i++) {
+        // set connection to itself as false
+        matrix[i][i] = false;
+
+        // create connection
+        setOfPoints[i].connect.forEach(connection => {
+            // ensure both way connection
+            matrix[i][connection.index] = true;
+            matrix[connection.index][i] = true;
+        });
+    }
+    return matrix;
+}
+
+function calculateAvgDistances(matrix, setOfPoints) {
+    for (let i = 0; i < matrix.length; i++) {
+        let sum = 0;
+        let count = 0;
+        for (let j = 0; j < matrix.length; j++) {
+            if (matrix[i][j]) {
+                count++;
+                sum += calcDistance(setOfPoints[i], setOfPoints[j]);
+            }
+        }
+        setOfPoints[i].avgDist = sum / count;
+    }
+    return setOfPoints;
+}
+
+function checkMatrixValidity(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        let count = 0;
+        for (let j = 0; j < matrix.length; j++) {
+            if (matrix[i][j]) {
+                count++;
+            }
+            if (count > n && matrix[i][j]) {
+                matrix[i][j] = false;
+                matrix[j][i] = false;
+            }
+        }
+    }
+    return matrix;
 }
 
 // returns array of lines to be drawn from current point
@@ -192,7 +315,7 @@ const findNearest = (grid, currentPoint) => {
 
     currentPoint.connect = [];
     if (distances.length == 0 || distances[0].dist == 0) { return currentPoint; }
-    if (!distances[0]) { console.log('HUJ'); return currentPoint; }
+    if (!distances[0]) { return currentPoint; }
 
     let pointsToDraw = [];
     for (let i = 0; i < n; i++) {
@@ -266,18 +389,5 @@ const generateGrid = (setOfPoints) => {
 
     return grid;
 }
-
-//takes in sorted by .x set of points
-const sqrMethod = (currentPoint, setOfPoints) => {
-    let delta = 10;
-    let square = {};
-    square.top = currentPoint.y - delta;
-    square.bot = currentPoint.y + delta;
-    square.left = currentPoint.x - delta;
-    square.righ = currentPoint.x + delta;
-    let found = []
-
-}
-
 
 window.addEventListener("load", main);
